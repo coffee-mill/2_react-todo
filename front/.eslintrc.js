5e73e76f9978b10f0e0799017904d685c9bfa339
Your Name <you@example.com>
2022-01-30 23:26:47 +0900

    #01 first commit

---
+++ front/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'space-before-function-paren': 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
